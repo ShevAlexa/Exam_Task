43df7aa2bf3e11e9dd3af3206c29e7105dc877db
ShevAlexa <zzxalex-chertiakazzx@mail.ru>
2022-02-05 22:38:20 +0300

    Add tests

---
+++ tests.py
@@ -0,0 +1,11 @@
+from tomatobush import TomatoBush
+from gardener import Gardener
+
+vegetable = TomatoBush(4)
+Gardener.knowledge_base()
+Gardener(vegetable).information()
+Gardener(vegetable).harvest()
+Gardener(vegetable).work()
+Gardener(vegetable).work()
+Gardener(vegetable).harvest()
+Gardener(vegetable).information()
